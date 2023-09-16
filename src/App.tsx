6a361531aef399216296053231f53407e9835017
papob.boonpat <papobboonpat@txm089557a.localdomain>
2023-09-16 22:51:44 +0700

    snap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { FixedSizeList } from "react-window";
+import { FixedSizeList, ListOnScrollProps } from "react-window";
 
 import classes from "./App.module.css";
-import { CSSProperties, useState } from "react";
+import { CSSProperties, useEffect, useRef, useState } from "react";
 import InfiniteLoader from "react-window-infinite-loader";
 import Moment from "moment";
 import { extendMoment } from "moment-range";
 const moment = extendMoment(Moment);
 function App() {
+  const loader = useRef<FixedSizeList<{ date: Moment.Moment }[]> | null>(null);
+  const debounce = useRef<number>();
   const [items, setItem] = useState(
     Array(14)
       .fill(null)
@@ -26,6 +28,18 @@ function App() {
     setItem((prev) => [...prev, ...newDates]);
   };
 
+  const onScroll = (prop: ListOnScrollProps) => {
+    clearTimeout(debounce.current);
+    debounce.current = setTimeout(() => {
+      loader.current?.scrollToItem(
+        Math.round(prop.scrollOffset / 100),
+        "start"
+      );
+    }, 50);
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <InfiniteLoader
@@ -43,8 +57,12 @@ function App() {
             onItemsRendered={onItemsRendered}
             width={window.innerWidth}
             layout="horizontal"
+            onScroll={onScroll}
             direction="rtl"
-            ref={ref}
+            ref={(list) => {
+              ref(list);
+              loader.current = list;
+            }}
           >
             {({
               index,
