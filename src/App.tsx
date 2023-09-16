bf573642a62efccd4ded4717a9f02361f1ccea8f
papob.boonpat <papobboonpat@txm089557a.localdomain>
2023-09-16 21:22:20 +0700

    implement date

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,63 @@
 import { FixedSizeList } from "react-window";
 
 import classes from "./App.module.css";
-import { CSSProperties, useEffect, useState } from "react";
+import { CSSProperties, useState } from "react";
 import InfiniteLoader from "react-window-infinite-loader";
 import Moment from "moment";
 import { extendMoment } from "moment-range";
 const moment = extendMoment(Moment);
 function App() {
-  const [items, setItem] = useState(Array(10).fill("load"));
-
-  useEffect(() => {
-    const formDate = moment();
-    const toDate = formDate.subtract(14, "days");
-
-    const range = moment().range(toDate, formDate);
-    console.log(Array.from(range.by("day")));
-  }, []);
+  const [items, setItem] = useState(
+    Array(14)
+      .fill(null)
+      .map((_, i) => ({
+        date: moment().subtract(i, "day"),
+      }))
+  );
 
   const isItemLoaded = (index: number) => !!items[index];
 
   const loadMoreItems = () => {
-    setItem((prev) => [...prev, ...Array(10).fill("new load")]);
+    const newDates = Array(7)
+      .fill(null)
+      .map((_, i) => ({
+        date: moment(items[items.length - 1].date).subtract(i + 1, "day"),
+      }));
+    setItem((prev) => [...prev, ...newDates]);
   };
+
   return (
     <div>
       <InfiniteLoader
         isItemLoaded={isItemLoaded}
-        itemCount={30}
+        itemCount={items.length + 1}
         loadMoreItems={loadMoreItems}
       >
         {({ onItemsRendered, ref }) => (
           <FixedSizeList
             className={classes["no-scrollbars"]}
             itemSize={100}
-            itemCount={30}
+            itemCount={items.length + 1}
             height={80}
+            itemData={items}
             onItemsRendered={onItemsRendered}
             width={window.innerWidth}
             layout="horizontal"
             direction="rtl"
             ref={ref}
           >
-            {({ index, style }: { index: number; style: CSSProperties }) => (
-              <div style={style}>{items[index]}</div>
+            {({
+              index,
+              style,
+              data,
+            }: {
+              index: number;
+              style: CSSProperties;
+              data: { date: Moment.Moment }[];
+            }) => (
+              <div style={style} className={classes.item}>
+                {data[index].date.format("DD/MM")}
+              </div>
             )}
           </FixedSizeList>
         )}
